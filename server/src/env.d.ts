c3793ee66105e16a3d106a80ced7d5bc46be5f65
João Geonizeli <joao.geonizeli@gmail.com>
2022-07-10 15:39:51 -0300

    move database configs to .env

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -1,5 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    DB_HOST?: string;
+    DB_USER?: string;
+    DB_PASSWORD?: string;
     DB_NAME?: string;
     NODE_ENV?: 'test' | 'development' | 'production';
   }
--- server/src/infra/dataSource.ts
+++ server/src/infra/dataSource.ts
@@ -1,21 +1,22 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Project } from "../entity/project.entity";
-import { Task } from '../entity/task.entity';
+import { Task } from "../entity/task.entity";
 import { User } from "../entity/user.entity";
 
-dotenv.config();
+dotenv.config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "joao",
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [User, Project, Task],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [User, Project, Task],
+});
